69ff234cde7bbcf8be2a5a25a19b843a93ca7076
yumanakayama <info@naka-y.com>
2024-10-12 15:55:08 +0900

    レイアウト調整。アカウント削除時モーダル表示作成。

--- app/components/button/AccountDeleteButton.tsx
+++ app/components/button/AccountDeleteButton.tsx
@@ -5,11 +5,12 @@ import styles from './css/accountDeleteButton.module.scss'
 
 const AccountDeleteButton = () => {
   const userAuth = authHook()
-  const [open, setOpen] = useState<boolean>(false)
+  const [modal, setModal] = useState<boolean>(false)
   const [password, setPassword] = useState<string>("")
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
     if (window.confirm("本当に削除しますか？")) {
       try {
         const response = await fetch(`/api/tenant/delete`, {
@@ -29,7 +30,7 @@ const AccountDeleteButton = () => {
           // トークンを削除する
           await fetch('/api/session/logout', { method: 'POST' })
           alert("アカウントを削除しました。")
-          setOpen(false)
+          setModal(false)
           setPassword("")
           window.location.href = "/";
         } else {
@@ -43,16 +44,23 @@ const AccountDeleteButton = () => {
 
   return (
     <>
-      <button type='button' onClick={() => setOpen(true)} className={styles.accountDeleteButton}>アカウント削除</button>
-      {open && (
-        <>
-          <p className={styles.cation}>削除する場合はパスワードを入力して”削除する”を押してください。</p>
-          <form onSubmit={handleSubmit} className={styles.form}>
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            <button type='submit' className={styles.deleteNow}>削除する</button>
-            <button type='button' onClick={() => setOpen(false)}>キャンセル</button>
-          </form>
-        </>
+      <button type='button' onClick={() => setModal(true)} className={styles.accountDeleteButton}>アカウント削除</button>
+      {modal && (
+        <div className={styles.deleteModal}>
+          <div className={styles.modalInner}>
+            <p className={styles.cation}>
+              テナントを削除します。<br />
+              削除する場合はパスワードを入力して<br />
+              ”削除する”を押してください。<br />
+              （データの復元はできません。）
+              </p>
+            <form onSubmit={handleSubmit} className={styles.form}>
+              <input type="password" className={styles.input} placeholder="パスワードを入力してください" value={password} onChange={e => setPassword(e.target.value)} />
+              <button type='button' className={styles.button} onClick={() => setModal(false)}>キャンセル</button>
+              <button type='submit' className={styles.button}>削除する</button>
+            </form>
+          </div>
+        </div>
       )}
     </>
   )
